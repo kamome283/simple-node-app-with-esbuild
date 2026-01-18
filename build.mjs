e5de4e284febd3ce1b38700a029e8232e0be76f8
Tsubasa <26428614+kamome283@users.noreply.github.com>
2026-01-18 15:14:18 +0900

    feat: ビルド関連の設定

---
+++ build.mjs
@@ -0,0 +1,26 @@
+import * as esbuild from "esbuild";
+
+try {
+    await esbuild.build({
+        entryPoints: ["src/index.ts"],
+        bundle: true,
+        platform: "node",
+        format: "cjs",
+        outfile: "dist/index.js",
+        sourcemap: true,
+        minify: true,
+    });
+    console.log("✅ ビルド成功");
+} catch (error) {
+    console.error("❌ ビルド失敗");
+    printErrorDescription(error);
+    process.exit(1)
+}
+
+function printErrorDescription(error) {
+    if (error?.message === undefined) {
+        console.error(error.message);
+    } else {
+        console.error(error);
+    }
+}
