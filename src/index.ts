d3201d496e0c9583365cff0f328183645758b648
Tsubasa <26428614+kamome283@users.noreply.github.com>
2026-01-18 15:36:35 +0900

    feat: `@types/node`の追加
    バージョンを25.xに指定しているが、ここは要検討

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import * as process from "node:process";
+
+const TOKEN = process.env["TOKEN"];
+if (TOKEN !== undefined) {
+  console.log(`TOKEN is ${TOKEN}`);
+} else {
+  console.log(`TOKEN not found in your .env`);
+}
